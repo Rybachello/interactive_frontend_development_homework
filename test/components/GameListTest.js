c051414474e117604e53735f279208d0912587bb
rybachello <mykolarybachello@gmail.com>
2017-04-20 21:00:33 +0300

    + gamelist test

--- test/components/GameListTest.js
+++ test/components/GameListTest.js
@@ -7,15 +7,15 @@ import WordGameApp from '../../src/components/WordGameApp';
 describe('GameList', () => {
     it('renders', () => {
         expect(shallow(
-            <GameList gameList={[]}/>
+            <GameList games={[]}/>
         )).to.exist;
     })
     it('renders number game when game type is number-game', () => {
-        const gameList = shallow(<GameList gameList={[{type:'number-game'}]}/>);
+        const gameList = shallow(<GameList games={[{type:'guess_number'}]}/>);
         expect(gameList).to.contain.descendants(NumberGameApp);
     });
     it('renders word game when game type is word-game', () => {
-        const gameList = shallow(<GameList gameList={[{type:'word-game'}]}/>);
+        const gameList = shallow(<GameList games={[{type:'guess_word'}]}/>);
         expect(gameList).to.contain.descendants(WordGameApp);
     });
 
